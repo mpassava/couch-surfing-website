a6b65fcca4352aa11d4fc29bd3cbbbe9c85fe9fd
mpassava <mpassava@gmail.com>
2023-10-30 01:11:29 -0400

    welcome back msg

--- index.js
+++ index.js
@@ -1,36 +1,49 @@
-// Number Types mini-challenge 10 10.2
-// Write a function that will only accept numbers and attend to 
-// all TypeScript weakness flags.
-// : number
-var reviewTotalDisplay = document.querySelector('#reviews');
+var returningUserDisplay = document.querySelector('#returning-user');
+var userNameDisplay = document.querySelector('#user');
+var reviewTotalDisplay = document.querySelector("#reviews");
 var reviews = [
     {
-        name: 'Sheia',
+        name: "Sheia",
         stars: 5,
         loyaltyUser: true,
-        date: '04-01-2021'
+        date: "04-01-2021",
     },
     {
-        name: 'Andrzej',
+        name: "Andrzej",
         stars: 3,
         loyaltyUser: false,
-        date: '03-28-2021'
+        date: "03-28-2021",
     },
     {
-        name: 'Omar',
+        name: "Omar",
         stars: 4,
         loyaltyUser: true,
-        date: '03-27-2021'
+        date: "03-27-2021",
     },
 ];
 var getLatestReview = function (arr) {
     var latestDate = Math.max.apply(Math, arr.map(function (r) { return new Date(r.date).getTime(); }));
-    var latestReview = arr.filter(function (r) { return (new Date(r.date)).getTime() === latestDate; })[0];
+    var latestReview = arr.filter(function (r) { return new Date(r.date).getTime() === latestDate; })[0];
     return latestReview;
 };
 function showReviewTotal(arrLen, _a) {
-    var name = _a.name;
-    reviewTotalDisplay.innerHTML = "Total Reviews ".concat(arrLen.toString(), " | Latest by ").concat(name);
+    var name = _a.name, loyaltyUser = _a.loyaltyUser;
+    reviewTotalDisplay.innerHTML = "Total Reviews ".concat(arrLen.toString(), " | Latest by ").concat(name, " ").concat(loyaltyUser ? '⭐' : '');
 }
 showReviewTotal(reviews.length, getLatestReview(reviews));
+var you = {
+    userName: {
+        firstName: 'Bobby',
+        lastName: 'Brown'
+    },
+    isReturning: true,
+};
+function populateUser(isReturning, _a) {
+    var firstName = _a.firstName, lastName = _a.lastName;
+    if (isReturning) {
+        returningUserDisplay.innerHTML = 'back';
+    }
+    userNameDisplay.innerHTML = "".concat(firstName, " ").concat(lastName);
+}
+populateUser(you.isReturning, you.userName);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,38 +1,61 @@
-// Number Types mini-challenge 10 10.2
-// Write a function that will only accept numbers and attend to 
-// all TypeScript weakness flags.
-// : number
-const reviewTotalDisplay = document.querySelector('#reviews')
+const returningUserDisplay = document.querySelector('#returning-user');
+const userNameDisplay = document.querySelector('#user');
+const reviewTotalDisplay = document.querySelector("#reviews");
 
 const reviews = [
-    {
-        name: 'Sheia',
-        stars: 5,
-        loyaltyUser: true,
-        date: '04-01-2021'
-    },
-    {
-        name: 'Andrzej',
-        stars: 3,
-        loyaltyUser: false,
-        date: '03-28-2021'
-    },
-    {
-        name: 'Omar',
-        stars: 4,
-        loyaltyUser: true,
-        date: '03-27-2021'
-    },
-]
+  {
+    name: "Sheia",
+    stars: 5,
+    loyaltyUser: true,
+    date: "04-01-2021",
+  },
+  {
+    name: "Andrzej",
+    stars: 3,
+    loyaltyUser: false,
+    date: "03-28-2021",
+  },
+  {
+    name: "Omar",
+    stars: 4,
+    loyaltyUser: true,
+    date: "03-27-2021",
+  },
+];
 
 const getLatestReview = (arr: Array<any>) => {
   const latestDate = Math.max(...arr.map(r => new Date(r.date).getTime()));
-  const latestReview = arr.filter(r => (new Date(r.date)).getTime() === latestDate)[0];
+  const latestReview = arr.filter(
+    r => new Date(r.date).getTime() === latestDate
+  )[0];
   return latestReview;
-}
+};
 
-function showReviewTotal(arrLen: number, { name }: { name: string }) {
-  reviewTotalDisplay.innerHTML = `Total Reviews ${arrLen.toString()} | Latest by ${name}`;
+function showReviewTotal(
+  arrLen: number,
+  { name, loyaltyUser }: { name: string, loyaltyUser: boolean }
+) {
+  reviewTotalDisplay.innerHTML = `Total Reviews ${arrLen.toString()} | Latest by ${name} ${
+    loyaltyUser ? '⭐' : ''
+}`;
 }
 
 showReviewTotal(reviews.length, getLatestReview(reviews));
+
+const you = {
+  userName: {
+    firstName: 'Bobby',
+    lastName: 'Brown'
+  },
+  isReturning: true,
+}
+
+
+function populateUser( isReturning: boolean, { firstName, lastName }: { firstName: string, lastName: string} ) {
+  if (isReturning){
+    returningUserDisplay.innerHTML = 'back'
+  }
+  userNameDisplay.innerHTML = `${firstName} ${lastName}`
+}
+
+populateUser( you.isReturning, you.userName )
